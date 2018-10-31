8ccc8e7d5d6afaabd7f38a64d906aa63b4cf0815
TopA-8453 <TopA-8453@DESKTOP-1OQA9S3>
2018-10-31 13:38:47 +0900

    Upload Unlock PDF

---
+++ OpenWebPage/OpenWebPage/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace OpenWebPage
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.helloButton);
+
+            button.Click += (sender, e) =>
+            {
+                var uri = Android.Net.Uri.Parse("http://naver.com");
+                var intent = new Intent(Intent.ActionView, uri);
+                StartActivity(intent);
+            };
+        }
+    }
+}
